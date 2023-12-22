c5f9658b8781f52e77f90d063cde53d37a5b0664
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-12-22 23:45:17 +0700

    fix slider brand

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -201,40 +201,6 @@ function Layout({ children }) {
                     },
                 ],
             });
-            $(".brand-active").slick({
-                infinite: true,
-                arrows: false,
-                slidesToShow: 6,
-                slidesToScroll: 1,
-                autoplay: true,
-                autoplaySpeed: 2000,
-                responsive: [
-                    {
-                        breakpoint: 1250,
-                        settings: {
-                            slidesToShow: 4,
-                        },
-                    },
-                    {
-                        breakpoint: 1000,
-                        settings: {
-                            slidesToShow: 3,
-                        },
-                    },
-                    {
-                        breakpoint: 800,
-                        settings: {
-                            slidesToShow: 2,
-                        },
-                    },
-                    {
-                        breakpoint: 500,
-                        settings: {
-                            slidesToShow: 1,
-                        },
-                    },
-                ],
-            });
             $(".owlcarousel").owlCarousel({
                 loop: true,
                 margin: 0,
